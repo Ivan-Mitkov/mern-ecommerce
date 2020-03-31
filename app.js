5a5ca38ffff5b20765f7982aba9f960c993b658a
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-03-31 08:16:10 +0300

    basic project setup

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+const userRoutes=require('./routes/user');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Node" });
-});
+//db
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>console.log("DB connected"));
+
+//routes
+app.use('/api',userRoutes)
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`app listen on port ${port}`));
