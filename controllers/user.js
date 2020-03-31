3e6e1d3ee0ea643fa331b50a627d0124d9b2c95f
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-03-31 18:30:37 +0300

    friendly error message

--- controllers/user.js
+++ controllers/user.js
@@ -1,13 +1,16 @@
 const User = require("../models/User");
+const { errorHandler } = require("../helpers/dbErrorHadler");
 
-exports.signup =  (req, res) => {
+exports.signup = (req, res) => {
   const user = new User(req.body);
-  user.save((err,user)=>{
-    if(err){
-      return res.status(400).json({error:err})
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({ error: errorHandler(err) });
     }
+    user.salt = undefined;
+    user.hashed_password = undefined;
     res.json({
       user
-    })
-  })
+    });
+  });
 };
