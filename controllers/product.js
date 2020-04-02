193d672d9cd57a176e5f3c71be5b28ed3760a23a
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-04-02 17:13:09 +0300

    some primitive  product fields validation

--- controllers/product.js
+++ controllers/product.js
@@ -11,11 +11,23 @@ exports.create = (req, res) => {
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
-      return res.status(400).json({ error: "Image coul not be uploaded" });
+      return res.status(400).json({ error: "Image could not be uploaded" });
     }
+
+    //check for all fields
+    const { name, description, price, quantity, shipping, category } = fields;
+    if (!name || !description || !price || !quantity || !shipping || !category) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+
     const product = new Product(fields);
     //photo is the name we send files from client if we named them image it will be files.image
     if (files.photo) {
+      console.log("Photo size: ", files.photo.size);
+      //size in bites
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({ error: "Image should be less than 1MB" });
+      }
       //this is from Product Model getting files from client side
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
