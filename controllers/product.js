607e00414a48b5d109112c1d098c44d6bdc3fc84
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-04-08 06:17:54 +0300

    product read, delete, update

--- controllers/product.js
+++ controllers/product.js
@@ -4,6 +4,84 @@ const _ = require("lodash");
 const Product = require("../models/product");
 const { errorHandler } = require("../helpers/dbErrorHadler");
 
+//Middleware to save product in req object, like user
+exports.productById = (req, res, next, id) => {
+  //id is from params
+  Product.findById(id).exec((err, product) => {
+    if (err || !product) {
+      return res.status(400).json({ error: "Product not found" });
+    }
+    //if there is product save it in req object
+    req.product = product;
+    return next();
+  });
+};
+
+//Get product, so we get productId as param
+// which make middleware run and save product in from req.product
+exports.read = (req, res) => {
+  //don't send the photo
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
+//Get product and user from Middlewares because every time when there is productId and userId
+//in params middlewares run
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err) {
+      return res.status(400).json({ error: errorHandler(err) });
+    }
+    res.json({ msg: "Product deleted" });
+  });
+};
+exports.update = (req, res) => {
+  const form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({ error: "Image could not be uploaded" });
+    }
+
+    //check for all fields
+    const { name, description, price, quantity, shipping, category } = fields;
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !quantity ||
+      !shipping ||
+      !category
+    ) {
+      return res.status(400).json({ error: "All fields are required" });
+    }
+    //Get product from req object
+    let product = req.product;
+    //put all data using lodash extend we can use {...} but for change
+    product = _.extend(product, fields);
+
+    //photo is the name we send files from client if we named them image it will be files.image
+    if (files.photo) {
+      console.log("Photo size: ", files.photo.size);
+      //size in bites
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({ error: "Image should be less than 1MB" });
+      }
+      //this is from Product Model getting files from client side
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+    //now save the product
+    product.save((err, data) => {
+      if (err) {
+        return res.status(400).json({ error: errorHandler(err) });
+      }
+      res.json({ data });
+    });
+  });
+};
+
 exports.create = (req, res) => {
   // https://www.npmjs.com/package/formidable
 
@@ -16,7 +94,14 @@ exports.create = (req, res) => {
 
     //check for all fields
     const { name, description, price, quantity, shipping, category } = fields;
-    if (!name || !description || !price || !quantity || !shipping || !category) {
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !quantity ||
+      !shipping ||
+      !category
+    ) {
       return res.status(400).json({ error: "All fields are required" });
     }
 
--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const { create } = require("../controllers/product");
+const { create, productById, read, remove,update } = require("../controllers/product");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
+router.get("/product/:productId", read);
 //only admin can create new category
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
+router.put(
+  "/product/:productId/:userId",
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
+);
+
+//MIDDLEWARES
 //when we have param "userId" we run this middleware and save user in req.profile
 router.param("userId", userById);
+//when we have param "productId" we run this middleware and save user in req.product
+router.param("productId", productById);
 module.exports = router;
