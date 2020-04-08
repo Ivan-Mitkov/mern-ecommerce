fc856379f7c5dbdbd0d5765d171e117e34bf21e1
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-04-08 07:22:39 +0300

    category by id and single category

--- controllers/category.js
+++ controllers/category.js
@@ -1,6 +1,12 @@
 const Category = require("../models/Category");
 const { errorHandler } = require("../helpers/dbErrorHadler");
 
+//Get category, so we get categoryId as param
+// which make middleware run and save category in req.category
+exports.read = (req, res) => {
+  //don't send the photo
+  return res.json(req.category);
+};
 exports.create = (req, res) => {
   const category = new Category(req.body);
   category.save((err, data) => {
@@ -8,5 +14,17 @@ exports.create = (req, res) => {
       return res.status(400).json({ error: errorHandler(err) });
     }
     res.json({ data });
+  });
+};
+exports.categoryById = (req, res, next, id) => {
+  //id is from params
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({ error: "Category does not exists" });
+    }
+    //if there is category save it in req object
+    req.category = category;
+    // console.log("Category", category);
+    return next();
   });
 };
--- routes/category.js
+++ routes/category.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { create } = require("../controllers/category");
+const { create,categoryById,read } = require("../controllers/category");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
+
+router.get("/category/:categoryId", read);
 
 //only admin can create new category
 router.post("/category/create/:userId", requireSignin, isAuth, isAdmin, create);
 //when we have param "userId" we run this middleware and save user in req.profile
 router.param("userId", userById);
+router.param("categoryId", categoryById);
 module.exports = router;
