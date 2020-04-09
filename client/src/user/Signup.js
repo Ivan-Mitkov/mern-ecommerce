6ca493fc75128aecac6c3d5d97edf10dc4db2239
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-04-09 08:02:54 +0300

    config.js file taking long named variables from .env file

--- client/src/user/Signup.js
+++ client/src/user/Signup.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Layout from "../core/Layout";
+import { API } from "../config";
 const Signup = () => {
   return (
     <Layout title="Signup" description="Signup to Node React E-commerce App">
       Signup
+      {API}
     </Layout>
   );
 };
