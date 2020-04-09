300aabbf5e1427bae86fb30067faf5970b7a140d
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-04-09 06:41:13 +0300

    client - basic react config, routes and pages

---
+++ client/src/Routes.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signup from "./user/Signup";
+import Signin from "./user/Signin";
+import Home from "./core/Home";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <div>Routes</div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signin" component={Signin} />
+        <Route exact path="/signup" component={Signup} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
