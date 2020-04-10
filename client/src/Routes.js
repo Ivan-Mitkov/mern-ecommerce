bc7a8bcd123e0f1ea41f119718b91ee99feb2f1c
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-04-10 12:15:00 +0300

    private route component pattern

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import Home from "./core/Home";
+import PrivateRoute from "./auth/PrivateRoute";
+import Dashboard from "./user/UserDashboard";
 
 const Routes = () => {
   return (
     <BrowserRouter>
-    
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={Signin} />
         <Route exact path="/signup" component={Signup} />
+        <PrivateRoute component={Dashboard} exact path="/dashboard" />
       </Switch>
     </BrowserRouter>
   );
---
+++ client/src/auth/PrivateRoute.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isAutenticated } from "./index";
+
+//https://reacttraining.com/react-router/web/example/auth-workflow
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAutenticated() ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/signin",
+              state: { from: props.location },
+            }}
+          />
+        )
+      }
+    />
+  );
+};
+
+export default PrivateRoute;
