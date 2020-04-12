da54b8569b43e342bf7ffaf0d0748ae5750fddf5
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-04-12 11:28:03 +0300

    handle checkbox

--- client/src/core/Checkbox.js
+++ client/src/core/Checkbox.js
@@ -1,9 +1,31 @@
 import React, { useEffect, useState } from "react";
 
 const Checkbox = ({ categories }) => {
+  const [checked, setChecked] = useState([]);
+  const handleToggle = (c) => () => {
+    //find if category in state
+    //return first index or -1
+    const currentCategoryId = checked.indexOf(c);
+    //copy state
+    const newCheckedCategoryId = [...checked];
+    //if currently checked not in state push in newChecked
+    if (currentCategoryId === -1) {
+      newCheckedCategoryId.push(c);
+      //else take off
+    } else {
+      newCheckedCategoryId.splice(currentCategoryId, 1);
+    }
+    console.log(newCheckedCategoryId);
+    setChecked(newCheckedCategoryId);
+  };
   return categories.map((c, i) => (
     <li key={i} className="list-unstyled">
-      <input type="checkbox" className="form-check-input" />
+      <input
+        value={checked.indexOf(c._id === -1)}
+        onChange={handleToggle(c._id)}
+        type="checkbox"
+        className="form-check-input"
+      />
       <label className="form-check-label">{c && c.name}</label>
     </li>
   ));
